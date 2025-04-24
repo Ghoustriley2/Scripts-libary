a299d12884c7490aea3eabdc562554c458d5e5fd
Ghoustriley2 <ghoustriley200915@gmail.com>
2025-04-24 10:16:29 +0300

    Update nightmode.js

--- docs/nightmode.js
+++ docs/nightmode.js
@@ -3,13 +3,12 @@
   const kievOffset = 3 * 60; // UTC+3
   const localOffset = now.getTimezoneOffset();
   const kievTime = new Date(now.getTime() + (kievOffset + localOffset) * 60000);
-  const hour = 3;
-
+  const hour = kievTime.getHours();
   if (hour === 3) {
     document.body.classList.add('witching-mode');
 
     document.querySelectorAll('h1, h2, h3').forEach(el => {
-      el.textContent = 'Они наблюдают...';
+      el.textContent = 'I see you...';
     });
 
     const glitch = document.createElement('div');
